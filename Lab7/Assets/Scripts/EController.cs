13a56df3eae7a36511878d7a09c44e5e0d91ec0c
Cristina B <bau161260@uvg.edu.gt>
2019-05-14 07:19:04 -0600

    Ya esta terminado

---
+++ Lab7/Assets/Scripts/EController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EController : MonoBehaviour
+{
+
+    public int moverEnX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D pegar = Physics2D.Raycast(transform.position, new Vector2(moverEnX, 0));
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moverEnX, 0) * 4;
+        if(pegar.distance < 0.8f)
+        {
+            if(moverEnX > 0)
+            {
+                moverEnX = -1;
+            }
+            else
+            {
+                moverEnX = 1;
+            }
+
+        }
+
+    }
+}
---
+++ Lab7/Assets/Scripts/PController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PController : MonoBehaviour
+{
+
+    private bool verDerecha = false;
+    private float moverX;
+    public Sprite nuevo;
+    private bool power = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoverJugador();
+    }
+
+    void MoverJugador()
+    {
+        moverX = Input.GetAxis("Horizontal");
+        if(Input.GetButtonDown("Jump"))
+        {
+            if(Mathf.Abs(GetComponent<Rigidbody2D>().velocity.y) < 0.5f)
+            {
+                GetComponent<Rigidbody2D>().AddForce(Vector2.up * 1250);
+            }
+
+        }
+
+        if(moverX < 0.0f && verDerecha == false)
+        {
+            CambiarVista();
+        } else if(moverX > 0.0f && verDerecha == true)
+        {
+            CambiarVista();
+        }
+
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moverX * 10, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void CambiarVista()
+    {
+        verDerecha = !verDerecha;
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Moneda")
+        {
+            Destroy(collision.gameObject);
+            GetComponent<SpriteRenderer>().sprite = nuevo;
+            GetComponent<Transform>().localScale = new Vector2(1.8f, 1.5f);
+            power = true;
+
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemigo")
+        {
+            if (power == true)
+            {
+                Destroy(collision.gameObject);
+            } else if(power == false)
+            {
+                Destroy(gameObject);
+            }
+
+        }
+    }
+
+
+
+}
